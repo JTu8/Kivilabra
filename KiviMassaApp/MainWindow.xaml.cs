e2742daac4c841067571529b8ae8a785d6623913
Jere Tuppurainen <Jere.Tuppurainen@edu.savonia.fi>
2018-06-11 13:46:54 +0300

    Eka commit

---
+++ KiviMassaApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KiviMassaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenKivi(object sender, RoutedEventArgs e)
+        {
+            Kiviohjelma k = new Kiviohjelma();
+            k.Show();
+        }
+
+        private void OpenMassa(object sender, RoutedEventArgs e)
+        {
+            Massaohjelma m = new Massaohjelma();
+            m.Show();
+        }
+    }
+}
