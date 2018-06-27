223e0ec63773efef58cab66a9cbdb771f9e8c08d
ValtteriKovSavonia <40198366+ValtteriKovSavonia@users.noreply.github.com>
2018-06-27 12:02:17 +0300

    Add files via upload

--- KiviMassaApp/Massaohjelma.xaml.cs
+++ KiviMassaApp/Massaohjelma.xaml.cs
@@ -97,8 +97,323 @@ namespace KiviMassaApp
 
         private void LaskeSeula_Click(object sender, RoutedEventArgs e)
         {
-            //Suoritetaan seulalaskut tässä
+            EmptyResultFields();//tyhjentää tuloskentät
+            int pyoristys = Convert.ToInt32(dbDesimaali.Text);//Otetaan valittu pyöristysarvo ohjelmasta
+            List<SyotetytArvot> syotetytarvot = new List<SyotetytArvot>(); //Luo listan johon syötetyt arvot tallennetaan
+            syotetytarvot = LuetaanSyotetytArvot(); //Luetaan syotetyt arvot luotuun listaan
+            double kokomassa = LaskeKokonaisMassa(syotetytarvot, pyoristys); //lasketaan arvojen kokonaismäärä
+            TulostenLasku_SeulalleJai(syotetytarvot, kokomassa, pyoristys); //Lasketaan laskutoimituksia ja syötetään tulokset tuloskenttiin
+            TulostenLasku_LapaisyProsentti(syotetytarvot, kokomassa, pyoristys);//--------||----------
+            Laskut l = new Laskut();
         }
+
+        private void TulostenLasku_LapaisyProsentti(List<SyotetytArvot> sa, double kokomassa, int pyoristys)
+        {
+            //Luodaan lista jonne tulokset laitetaan
+            List<SyotetytArvot> tulos = new List<SyotetytArvot>();
+            Laskut laskut = new Laskut();
+            //Lasketaan ja syötetään tulokset tulos-listaan
+            tulos = laskut.LapaisyProsentti(sa, kokomassa);
+            //Käydään lista läpi ja tulostetaan tulokset oikeille kentille
+            foreach (SyotetytArvot s in tulos)
+            {
+                switch (s.index)
+                {
+                    case 1:
+                        lapaisypros1.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 2:
+                        lapaisypros2.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 3:
+                        lapaisypros3.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 4:
+                        lapaisypros4.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 5:
+                        lapaisypros5.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 6:
+                        lapaisypros6.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 7:
+                        lapaisypros7.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 8:
+                        lapaisypros8.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 9:
+                        lapaisypros9.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 10:
+                        lapaisypros10.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 11:
+                        lapaisypros11.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 12:
+                        lapaisypros12.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 13:
+                        lapaisypros13.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 14:
+                        lapaisypros14.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 15:
+                        lapaisypros15.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    case 16:
+                        lapaisypros16.Text = Math.Round(Convert.ToDouble(s.syote), pyoristys).ToString();
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("VIRHE Kiviohjelma.xaml.cs tiedostossa: lapaisypros-kenttiä täydentäessä tuli vastaan outo index luku SyotetytArvot-listasta 'tulos'");
+                            break;
+                        }
+                }
+            }
+
+        }
+        private void TulostenLasku_SeulalleJai(List<SyotetytArvot> sa, double kokomassa, int pyoristys)
+        {
+            for (int l = 0; l < sa.Count; l++)
+            {
+                //otetaan ja lasketaan yksi tulos
+                Laskut laskut = new Laskut();
+                double tulos = Math.Round(laskut.seulalleJai(Convert.ToDouble(sa[l].syote), kokomassa), pyoristys);
+
+                //Valitaan oikea paikka tulokselle sen indeksin perusteella
+                switch (sa[l].index)
+                {
+                    case 1:
+                        seulapros1.Text = tulos.ToString();
+                        break;
+                    case 2:
+                        seulapros2.Text = tulos.ToString();
+                        break;
+                    case 3:
+                        seulapros3.Text = tulos.ToString();
+                        break;
+                    case 4:
+                        seulapros4.Text = tulos.ToString();
+                        break;
+                    case 5:
+                        seulapros5.Text = tulos.ToString();
+                        break;
+                    case 6:
+                        seulapros6.Text = tulos.ToString();
+                        break;
+                    case 7:
+                        seulapros7.Text = tulos.ToString();
+                        break;
+                    case 8:
+                        seulapros8.Text = tulos.ToString();
+                        break;
+                    case 9:
+                        seulapros9.Text = tulos.ToString();
+                        break;
+                    case 10:
+                        seulapros10.Text = tulos.ToString();
+                        break;
+                    case 11:
+                        seulapros11.Text = tulos.ToString();
+                        break;
+                    case 12:
+                        seulapros12.Text = tulos.ToString();
+                        break;
+                    case 13:
+                        seulapros13.Text = tulos.ToString();
+                        break;
+                    case 14:
+                        seulapros14.Text = tulos.ToString();
+                        break;
+                    case 15:
+                        seulapros15.Text = tulos.ToString();
+                        break;
+                    case 16:
+                        seulapros16.Text = tulos.ToString();
+                        break;
+                    case 17:
+                        seulapros17.Text = tulos.ToString();
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("VIRHE Kiviohjelma.xaml.cs tiedostossa: seulapros-kenttiä täydentäessä tuli vastaan outo index luku SyotetytArvot-listasta");
+                            break;
+                        }
+                }
+
+            }
+        }
+
+        private double LaskeKokonaisMassa(List<SyotetytArvot> sa, int pyoristys)
+        {
+            //laskee syotettyjen arvojen kokonaismassan ja palauttaa sen
+            //Listassa on arvot jotka lasketaan yhteen, ja pyoristys-parametri kertoo kuinka tarkasti arvo pyöristetään
+            double kokomassa = 0;
+            foreach (SyotetytArvot se in sa)
+            {
+                kokomassa += Convert.ToDouble(se.syote);
+            }
+            punnittuYhteensa.Text = Math.Round(kokomassa, pyoristys).ToString();
+            return Math.Round(kokomassa, pyoristys);
+        }
+
+
+        private List<SyotetytArvot> LuetaanSyotetytArvot()
+        {
+            //Lukee käyttäjän syöttämät arvot, lisää ne listaan ja palauttaa listan
+            //Syötetyissä arvoissa saattaa olla välejä (kaikkeja rivejä ei täytetty) joten koodi tarkistaa sen myös
+            List<SyotetytArvot> sa = new List<SyotetytArvot>();
+            double g0, g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18;
+            try
+            {
+                if (double.TryParse(seulaG0.Text, out g0))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g0 = Convert.ToDouble(seulaG0.Text);
+                    s.syote = g0;
+                    s.index = 1;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG1.Text, out g1))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g1 = Convert.ToDouble(seulaG1.Text);
+                    s.syote = g1;
+                    s.index = 2;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG2.Text, out g2))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g2 = Convert.ToDouble(seulaG2.Text);
+                    s.syote = g2;
+                    s.index = 3;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG3.Text, out g3))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g3 = Convert.ToDouble(seulaG3.Text);
+                    s.syote = g3;
+                    s.index = 4;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG4.Text, out g4))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g4 = Convert.ToDouble(seulaG4.Text);
+                    s.syote = g4;
+                    s.index = 5;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG5.Text, out g5))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g5 = Convert.ToDouble(seulaG5.Text);
+                    s.syote = g5;
+                    s.index = 6;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG6.Text, out g6))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g6 = Convert.ToDouble(seulaG6.Text);
+                    s.syote = g6;
+                    s.index = 7;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG7.Text, out g7))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g7 = Convert.ToDouble(seulaG7.Text);
+                    s.syote = g7;
+                    s.index = 8;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG8.Text, out g8))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g8 = Convert.ToDouble(seulaG8.Text);
+                    s.syote = g8;
+                    s.index = 9;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG9.Text, out g9))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g9 = Convert.ToDouble(seulaG9.Text);
+                    s.syote = g9;
+                    s.index = 10;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG10.Text, out g10))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g10 = Convert.ToDouble(seulaG10.Text);
+                    s.syote = g10;
+                    s.index = 11;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG11.Text, out g11))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g11 = Convert.ToDouble(seulaG11.Text);
+                    s.syote = g11;
+                    s.index = 12;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG12.Text, out g12))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g12 = Convert.ToDouble(seulaG12.Text);
+                    s.syote = g12;
+                    s.index = 13;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG13.Text, out g13))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g13 = Convert.ToDouble(seulaG13.Text);
+                    s.syote = g13;
+                    s.index = 14;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG14.Text, out g14))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g14 = Convert.ToDouble(seulaG14.Text);
+                    s.syote = g14;
+                    s.index = 15;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG15.Text, out g15))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g15 = Convert.ToDouble(seulaG15.Text);
+                    s.syote = g15;
+                    s.index = 16;
+                    sa.Add(s);
+                }
+                if (double.TryParse(seulaG16.Text, out g16))
+                {
+                    SyotetytArvot s = new SyotetytArvot();
+                    g16 = Convert.ToDouble(seulaG16.Text);
+                    s.syote = g16;
+                    s.index = 17;
+                    sa.Add(s);
+                }
+            }
+            catch
+            {
+
+            }
+            return sa;
+        }
+
 
 
         private void SeulaArvotOhjeArvoihin()
